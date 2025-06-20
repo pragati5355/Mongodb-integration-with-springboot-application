55790fde5c5fcd44c455a1d0ba24233c697fc8d5
Pragati Gawade <pragati.gawade@mindbowser.com>
2025-06-20 19:27:42 +0530

    Update ProjectRepository.java

--- src/main/java/com/specification_demo/demo/repository/ProjectRepository.java
+++ src/main/java/com/specification_demo/demo/repository/ProjectRepository.java
@@ -3,14 +3,10 @@ package com.specification_demo.demo.repository;
 import org.springframework.stereotype.Repository;
 
 import com.specification_demo.demo.entity.Project;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
 import jakarta.transaction.Transactional;
 
 @Transactional
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long>, JpaSpecificationExecutor<Project>{
+public interface ProjectRepository extends MongoRepository<Project, String>, CustomProjectRepository{
     
 }
